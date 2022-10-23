379f5f2ec2e68ee7e076d1194c04c5f77e822e1c
Matej Poletar <matej.poletar@gmail.com>
2022-10-23 15:31:48 +0200

    Implement editor, options and toolbar for card overlay component

--- wp-content/themes/infinum-academy/src/Blocks/components/card-overlay/components/card-overlay-options.js
+++ wp-content/themes/infinum-academy/src/Blocks/components/card-overlay/components/card-overlay-options.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import { __ } from '@wordpress/i18n';
-import { PanelBody } from '@wordpress/components';
+import { props, getOptions } from '@eightshift/frontend-libs/scripts';
+import { ImageOptions } from '../../image/components/image-options';
+import { HeadingOptions } from '../../heading/components/heading-options';
+import { ParagraphOptions } from '../../paragraph/components/paragraph-options';
+import manifest from './../manifest.json';
 
-export const CardOverlayOptions = () => {
+export const CardOverlayOptions = (attributes) => {
 	return (
-		<PanelBody title={__('Card Overlay', 'infinum-academy')}>
-		</PanelBody>
+		<>
+			<ImageOptions
+				{...props('image', attributes)}
+				showImageUse
+				showLabel
+			/>
+
+			<HeadingOptions
+				{...props('heading', attributes, {
+					options: getOptions(attributes, manifest),
+				})}
+				showHeadingUse
+				showLabel
+			/>
+
+			<ParagraphOptions
+				{...props('paragraph', attributes, {
+					options: getOptions(attributes, manifest),
+				})}
+				showParagraphUse
+				showLabel
+			/>
+		</>
 	);
 };
