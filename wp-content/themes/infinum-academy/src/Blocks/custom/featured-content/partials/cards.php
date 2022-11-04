e3b127639ac383dadd6984bbb789bdfc1446c59c
Matej Poletar <matej.poletar@gmail.com>
2022-11-04 11:00:20 +0100

    Render data from api call and show it in feature content

---
+++ wp-content/themes/infinum-academy/src/Blocks/custom/featured-content/partials/cards.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Template for the Featured Content view - Card content map from ID.
+ *
+ * @package InfinumAcademy
+ */
+
+use InfinumAcademyVendor\EightshiftLibs\Helpers\Components;
+
+$items = $attributes['items'] ?? [];
+$blockSsr = $attributes['blockSsr'] ?? '';
+
+$output = [];
+
+if (!$items) {
+	return $output;
+}
+
+foreach ($items as $item) {
+	if (is_int($item)) {
+		$attributes = [
+			'cardHeadingContent' => get_the_title($item),
+			'cardParagraphContent' => get_the_excerpt($item),
+			'cardButtonUrl' => get_the_permalink($item),
+			'cardButtonContent' => __('View More', 'infinum-academy'),
+			'cardImageUrl' => get_the_post_thumbnail_url($item, 'large'),
+			'blockSsr' => $blockSsr,
+		];
+	}
+	$item = (array) $item;
+	$authors = (array) $item['authors'];
+	$firstAuthor = !empty($authors) ? (array) $authors[0] : [];
+	$formats = (array) $item['formats'];
+
+	$attributes = [
+		'cardHeadingContent' => $item['title'],
+		'cardParagraphContent' => $firstAuthor['name'] ?? '',
+		'cardButtonUse' => false,
+		'cardImageUrl' => $formats['image/jpeg'],
+		'blockSsr' => $blockSsr,
+	];
+
+	echo Components::render(
+		'card',
+		Components::props(
+			'card',
+			$attributes
+		),
+		'',
+		true
+	);
+}
